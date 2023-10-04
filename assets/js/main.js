5a919484090b1363e97cb5845445ce7e9a07b743
Keith Omondi Onyango <142302839+OnyangoOmondie97@users.noreply.github.com>
2023-10-04 19:06:14 +0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,41 +1,64 @@
-
 (function($) {
 
-	var	$window = $(window),
-		$banner = $('#banner'),
-		$body = $('body');
+    var $window = $(window),
+        $banner = $('#banner'),
+        $body = $('body');
 
-	// Breakpoints.
-		breakpoints({
-			default:   ['1681px',   null       ],
-			xlarge:    ['1281px',   '1680px'   ],
-			large:     ['981px',    '1280px'   ],
-			medium:    ['737px',    '980px'    ],
-			small:     ['481px',    '736px'    ],
-			xsmall:    ['361px',    '480px'    ],
-			xxsmall:   [null,       '360px'    ]
-		});
+    // Breakpoints.
+    breakpoints({
+        default: ['1681px', null],
+        xlarge: ['1281px', '1680px'],
+        large: ['981px', '1280px'],
+        medium: ['737px', '980px'],
+        small: ['481px', '736px'],
+        xsmall: ['361px', '480px'],
+        xxsmall: [null, '360px']
+    });
 
-	// Play initial animations on page load.
-		$window.on('load', function() {
-			window.setTimeout(function() {
-				$body.removeClass('is-preload');
-			}, 100);
-		});
+    // Play initial animations on page load.
+    $window.on('load', function() {
+        window.setTimeout(function() {
+            $body.removeClass('is-preload');
+        }, 100);
+    });
 
-	// Menu.
-		$('#menu')
-			.append('<a href="#menu" class="close"></a>')
-			.appendTo($body)
-			.panel({
-				target: $body,
-				visibleClass: 'is-menu-visible',
-				delay: 500,
-				hideOnClick: true,
-				hideOnSwipe: true,
-				resetScroll: true,
-				resetForms: true,
-				side: 'right'
-			});
+    // Video playback on scroll.
+    var $video = $("video");  // Ensure your video has a unique selector if there are multiple videos on the page.
+    $window.on('scroll', function() {
+        var videoTop = $video.offset().top;
+        var windowBottom = $window.scrollTop() + $window.height();
+
+        if (windowBottom > videoTop) {
+            $video[0].play();
+        } else {
+            $video[0].pause();
+        }
+    });
+
+    // Text appearing word by word.
+    var options = {
+        strings: ["Software Developer,", "Security Analyst &", "Molecular Biologist"],
+        typeSpeed: 50,
+        backDelay: 500,
+        backSpeed: 30,
+        loop: false
+    }
+
+    var typed = new Typed(".inner h1", options);
+
+    // Menu.
+    $('#menu')
+        .append('<a href="#menu" class="close"></a>')
+        .appendTo($body)
+        .panel({
+            target: $body,
+            visibleClass: 'is-menu-visible',
+            delay: 500,
+            hideOnClick: true,
+            hideOnSwipe: true,
+            resetScroll: true,
+            resetForms: true,
+            side: 'right'
+        });
 
 })(jQuery);
